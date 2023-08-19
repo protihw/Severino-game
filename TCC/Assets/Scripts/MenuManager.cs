aa8f8f1aa4927dbe900d4f89f5ba656c1798d249
proti <bernardo.proti@hotmail.com>
2023-08-19 17:30:02 -0300

    Funcionamento do menu

---
+++ TCC/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject mainMenu, optionsMenu, creditsMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void ChangeSceneSampleScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ChangePanelMain()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+    }
+
+    public void ChangePanelOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        creditsMenu.SetActive(false);
+    }
+
+    public void ChangePanelCredits()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
