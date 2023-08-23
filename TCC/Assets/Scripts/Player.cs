1cfffafb3c8324eb33f438a0c67a6dbd90e31a7e
proti <bernardo.proti@hotmail.com>
2023-08-22 23:48:11 -0300

    Movimentação incluida

--- TCC/Assets/Scripts/Player.cs
+++ TCC/Assets/Scripts/Player.cs
@@ -4,49 +4,58 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;  // Velocidade de movimento do boneco
-    private Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
     public Animator animator;
+    // public GameObject leve1, level2, level3, level4, level5, level6;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         bool isPressed = false;
+        Vector2 movement = new Vector2(0, 0);
 
         // Botão W
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("Botão W pressionado.");
+            movement.y = 1;
             isPressed = true;
+            Debug.Log("Botão W pressionado.");
         }
 
         // Botão A
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Botão A pressionado");
+            movement.x = -1;
             transform.rotation = Quaternion.Euler(0f, 180f, 0f);
             isPressed = true;
+            Debug.Log("Botão A pressionado");
         }
 
         // Botão S
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("Botão S pressionado.");
+            movement.y = -1;
             isPressed = true;
+            Debug.Log("Botão S pressionado.");
         }
         
         // Botão D
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Botão D pressionado.");
+            movement.x = 1;
             transform.rotation = Quaternion.Euler(0f, 0f, 0f);
             isPressed = true;
+            Debug.Log("Botão D pressionado.");
         }
+        
+        movement.Normalize();
+        rb.velocity = movement * moveSpeed;
 
         animator.SetBool("isPressed", isPressed);
     }
