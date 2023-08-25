5d40ea21d4b2c0e6266548770959dd9024cee129
protihw <69305991+protihw@users.noreply.github.com>
2023-08-25 10:58:53 -0300

    Revert "code"
    This reverts commit 2ce309a4141ad68d66a4995f8aacb8b018e6b773.

--- TCC/Assets/Scripts/Player.cs
+++ TCC/Assets/Scripts/Player.cs
@@ -7,8 +7,7 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Animator animator;
-    public Transform[] waypoints;
-    private int nextWaypoint = 0;
+    // public GameObject leve1, level2, level3, level4, level5, level6;
 
     void Start()
     {
@@ -21,88 +20,42 @@ public class Player : MonoBehaviour
         bool isPressed = false;
         Vector2 movement = new Vector2(0, 0);
 
-        Transform waypoint = waypoints[nextWaypoint];
-
-        if (transform.position.x < waypoint.position.x)
+        // Botão W
+        if (Input.GetKey(KeyCode.W))
         {
-            // Botão D
-            if (Input.GetKey(KeyCode.D))
-            {
-                movement.x = 1;
-                isPressed = true;
-                Debug.Log("Botão D pressionado.");
-                if (Input.GetKey(KeyCode.A))
-                {
-                    movement.x = -1;
-                    isPressed = true;
-                    Debug.Log("Botão A pressionado");
-                }
-            }
+            movement.y = 1;
+            isPressed = true;
+            Debug.Log("Botão W pressionado.");
         }
 
-        if (transform.position.x > waypoint.position.x)
+        // Botão A
+        if (Input.GetKey(KeyCode.A))
         {
-            // Botão A
-            if (Input.GetKey(KeyCode.A))
-            {
-                movement.x = -1;
-                isPressed = true;
-                Debug.Log("Botão A pressionado");
-                if (Input.GetKey(KeyCode.D))
-                {
-                    movement.x = 1;
-                    isPressed = true;
-                    Debug.Log("Botão D pressionado.");
-                }
-            }
+            movement.x = -1;
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            isPressed = true;
+            Debug.Log("Botão A pressionado");
         }
 
-        if (transform.position.y < waypoint.position.y)
+        // Botão S
+        if (Input.GetKey(KeyCode.S))
         {
-            // Botão W
-            if (Input.GetKey(KeyCode.W))
-            {
-                movement.y = 1;
-                isPressed = true;
-                Debug.Log("Botão W pressionado.");
-                if (Input.GetKey(KeyCode.S))
-                {
-                    movement.y = -1;
-                    isPressed = true;
-                    Debug.Log("Botão S pressionado.");
-                }
-            }
+            movement.y = -1;
+            isPressed = true;
+            Debug.Log("Botão S pressionado.");
         }
-
-        if (transform.position.y > waypoint.position.y)
+        
+        // Botão D
+        if (Input.GetKey(KeyCode.D))
         {
-            // Botão S
-            if (Input.GetKey(KeyCode.S))
-            {
-                movement.y = -1;
-                isPressed = true;
-                Debug.Log("Botão S pressionado.");
-                if (Input.GetKey(KeyCode.W))
-                {
-                    movement.y = 1;
-                    isPressed = true;
-                    Debug.Log("Botão W pressionado.");
-                }
-            }
+            movement.x = 1;
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            isPressed = true;
+            Debug.Log("Botão D pressionado.");
         }
-
+        
         movement.Normalize();
         rb.velocity = movement * moveSpeed;
-
-        if (Vector2.Distance(transform.position, waypoint.position) < 0.1f)
-        {
-            nextWaypoint++;
-        }
-
-        if (Vector2.Distance(Transform.positionm, waypoint.position) > 0.1f)
-        {
-            nextWaypoint--;
-        }
 
         animator.SetBool("isPressed", isPressed);
     }
