0d1922b8f347e82bbed5be2c08abbfaea608aad7
proti <bernardo.proti@hotmail.com>
2023-08-30 19:44:22 -0300

    Update ColectGems.cs

--- TCC/Assets/Scripts/ColectGems.cs
+++ TCC/Assets/Scripts/ColectGems.cs
@@ -1,13 +1,10 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ColectGems : MonoBehaviour
 public class CollectGems : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
     public Text scoreTxt;
     private int score;
 
@@ -18,15 +15,11 @@ public class CollectGems : MonoBehaviour
 
     private void Update()
     {
-        
         scoreTxt.text = score.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
     private void OnTriggerEnter2D(Collider2D col)
     {
-        
         if (col.CompareTag("coin"))
         {
             score = score + 1;
