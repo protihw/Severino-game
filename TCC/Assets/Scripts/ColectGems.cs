a2f7f0de9eb127d8697607aeaef5edbc4673ec3a
Vicenzo <vicenzofr@gmail.com>
2023-08-30 15:07:00 -0300

    aba

--- TCC/Assets/Scripts/ColectGems.cs
+++ TCC/Assets/Scripts/ColectGems.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ColectGems : MonoBehaviour
+public class CollectGems : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text scoreTxt;
+    private int score;
+
+    private void Start()
     {
-        
+        score = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        scoreTxt.text = score.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("coin"))
+        {
+            score = score + 1;
+            Destroy(col.gameObject);
+        }
     }
 }
